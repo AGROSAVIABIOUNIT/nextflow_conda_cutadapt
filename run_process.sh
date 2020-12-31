32b6677887b0d275f014ef77f8b7d33e047cb995
AGROSAVIABIOUNIT <agrosavia.bioinformatics@gmail.com>
2020-12-31 17:24:07 -0500

    commit_adapters_contamination

--- run_process.sh
+++ run_process.sh
@@ -4,7 +4,7 @@ data_dir=/home/path/to/data/parent/directory
 nextflow run ${nextflow_workflows_dir}/cutadap_trimmo.nf \
 --adapter_file ${nextflow_workflows_dir}/adapters.fas \
 --input_dir ${data_dir}/fastqs \
---fastq_pattern '*.R{1,2}.f*q.gz' \
+--fastq_pattern '*{_,R}{1,2}.f*q.gz' \
 --output_dir ${data_dir}/cutadapt_trimmo_output \
 -resume \
 -ansi \
